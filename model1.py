527f5e2d7c6b2e7dbfe2bca5ddfc89c96817840c
nootawa <naikiotawa@gmail.com>
2024-06-29 21:29:22 +0900

    画像url更新2

--- model1.py
+++ model1.py
@@ -3,9 +3,7 @@ from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.preprocessing import image
-
 import numpy as np
-
 
 
 # ねじれ:classes = ["正常","異常"]
@@ -21,7 +19,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 model = load_model('./casting_defect_model1_4.h5')#学習済みモデルをロード
-
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -49,7 +46,6 @@ def upload_file():
             pred_answer = "アップロードされた画像は、 " + classes[predicted] + " な製品画像です"
             return render_template("index.html",answer=pred_answer)
     return render_template("index.html",answer="")
-
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
