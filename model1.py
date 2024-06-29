08dfd78ca2637a7b9d130d2896fa8f321aa48037
nootawa <naikiotawa@gmail.com>
2024-06-29 21:41:00 +0900

    更新1

--- model1.py
+++ model1.py
@@ -36,7 +36,7 @@ def upload_file():
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
             #受け取った画像を読み込み、np形式に変換
-            # 今回モデルはグレースケール指定ではないため、color_mode="grayscale"無指定
+            #今回モデルはグレースケール指定ではないため、color_mode="grayscale"無指定
             img = image.load_img(filepath, target_size=(image_size,image_size))
             img = image.img_to_array(img)
             data = np.array([img])
